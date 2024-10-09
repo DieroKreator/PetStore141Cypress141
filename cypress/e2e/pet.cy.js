67da4f1161d7e5ad2ad342380065d440119d26c6
DieroKreator <qtechwired@gmail.com>
2024-10-09 14:57:17 -0300

    adicionei teste put e delete

--- cypress/e2e/pet.cy.js
+++ cypress/e2e/pet.cy.js
@@ -1,5 +1,6 @@
 // Bibliotecas
 import pet from '../fixtures/pet.json'
+import petAlterado from '../fixtures/petAlterado.json'
 
 describe('CRUD da entidade Pet', () => {
 
@@ -39,7 +40,34 @@ describe('CRUD da entidade Pet', () => {
   })
 
   // Put Pet
+  it('PUT Pet', () => {
+    cy.request({
+      method: 'PUT',
+      url: '/pet',
+      body: petAlterado
+    }).then(({ status, body }) => {
+      expect(status).to.eq(200)
+      expect(body.id).to.eq(petAlterado.id)
+      expect(body.name).to.eq(petAlterado.name)
+      expect(body.category.id).to.eq(petAlterado.category.id)
+      expect(body.category.name).to.eq(petAlterado.category.name)
+      expect(body.tags[0].id).to.eq(petAlterado.tags[0].id)
+      expect(body.tags[0].name).to.eq(petAlterado.tags[0].name)
+      expect(body.status).to.eq(petAlterado.status)
+    })
+  })
 
   // Delete Pet
-
+  it('DELETE Pet', () => {
+    cy.request({
+      method: 'DELETE',
+      url: `/pet/${pet.id}`
+    }).then(({ status, body }) => {
+      expect(status).to.eq(200)
+      expect(body.code).to.eq(200)
+      expect(body.type).to.eq('unknown')
+      expect(body.message).to.eq(`${pet.id}`)
+      // expect(body.message).to.eq((pet.id).toString())
+    })
+  })
 })
