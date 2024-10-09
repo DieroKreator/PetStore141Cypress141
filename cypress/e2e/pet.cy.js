e7e90cb5a97beb8249ba7b5338f90661b0654466
DieroKreator <qtechwired@gmail.com>
2024-10-09 17:06:28 -0300

    post com massa

--- cypress/e2e/pet.cy.js
+++ cypress/e2e/pet.cy.js
@@ -1,6 +1,7 @@
 // Bibliotecas
 import pet from '../fixtures/pet.json'
 import petAlterado from '../fixtures/petAlterado.json'
+import listaPets from '../fixtures/listaPets.json'
 
 describe('CRUD da entidade Pet', () => {
 
@@ -70,4 +71,25 @@ describe('CRUD da entidade Pet', () => {
       // expect(body.message).to.eq((pet.id).toString())
     })
   })
+
+  listaPets.forEach((element) => {
+    // Post Pet Data Driven
+    it(`POST Pet Data Driven - ${element.name}`, () => {
+      cy.request({
+        method: 'POST',
+        url: '/pet',
+        body: element
+      }).then(({ status, body }) => {
+        expect(status).to.eq(200)
+        expect(body.id).to.eq(element.id)
+        expect(body.name).to.eq(element.name)
+        expect(body.category.id).to.eq(element.category.id)
+        expect(body.category.name).to.eq(element.category.name)
+        expect(body.tags[0].id).to.eq(element.tags[0].id)
+        expect(body.tags[0].name).to.eq(element.tags[0].name)
+        expect(body.status).to.eq(element.status)
+      })
+    })
+  })
+
 })
